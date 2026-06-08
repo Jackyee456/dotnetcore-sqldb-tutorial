5f00f5734f94cb64cfa678a1e9ef0ba148537444
Jackyee456 <jackioishopify@gmail.com>
2026-06-09 11:42:17 +1200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using DotNetCoreSqlDb.Models;
+using DotNetCoreSqlDb.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +9,7 @@ builder.Logging.AddAzureWebAppDiagnostics();
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
 
 var app = builder.Build();
 
@@ -34,5 +34,11 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Todos}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<MyDatabaseContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
